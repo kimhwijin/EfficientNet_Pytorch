4b076f4fc8de9d161e54ac9ec8847bbc101a033c
kimhwijin <rlagnlwls0160@gmail.com>
2022-07-25 13:32:03 +0900

    utils: block decoder

--- EfficientNet/utils.py
+++ EfficientNet/utils.py
@@ -1,9 +1,11 @@
 import math
 import collections
+from numpy import isin
 import torch
 from torch import nn
 from torch.nn import functional as F
 from functools import partial
+import re
 
 GlobalParams = collections.namedtuple("GlobalParams", 
 [
@@ -268,3 +270,108 @@ def get_same_padding_maxPool2d(image_size=None):
         return MaxPool2dDynamicSamePadding
     else:
         return partial(MaxPool2dStaticSamePadding, image_size=image_size)
+    
+
+class BlockDecoder(object):
+    """Block Decoder for readability.
+    """
+
+    @staticmethod
+    def _decode_block_string(block_string):
+        """Get a block through a string notation of arguments.
+        
+        Args:
+            block_string (str): A string notation of arguments.
+                Examles: 'r1_k3_s11_e1_i32_o16_se0.25_noskip'.
+        
+        Returns:
+            BlockArgs: The namedTuple defined at the top of this file.
+        """
+
+        assert isinstance(block_string, str)
+
+        ops = block_string.split('_')
+        options = {}
+        for op in ops:
+            splits = re.split(r'(\d.*)', op)
+            if len(splits) >= 2:
+                key, value = splits[:2]
+                options[key] = value
+        
+        assert (
+            ('s' in options and len(options['s']) == 1) or 
+            (len(options['s']) == 2 and options['s'][0] == options['s'][1])
+        )
+
+        return BlockArgs(
+            num_repeat=int(options['r']),
+            kernel_size=int(options['k']),
+            stride=[int(options['s'][0])],
+            expand_ratio=int(options['e']),
+            input_filters=int(options['i']),
+            output_filters=int(options['o']),
+            se_ratio = float(options['se']) if 'se' in options else None,
+            id_skip=('noskip' not in block_string)
+        )
+    
+    @staticmethod
+    def _encode_block_string(block):
+        """Encode a block to string
+        
+        Args:
+            block (namedTuple): A BlockArgs type argument.
+        
+        Returns:
+            block_strings: A String form of BlockArgs.
+        """
+
+        args = [
+            'r%d' % block.num_repeat,
+            'k%d' % block.kernel_size,
+            's%d%d' % (block.strides[0], block.strides[1]),
+            'e%s' % block.expand_ratio,
+            'i%d' % block.input_filters,
+            'o%d' % block.output_filters,
+        ]
+
+        if 0 < block.se_ratio <= 1:
+            args.append('se%s' % block.se_ratio)
+        if block.id_skip is False:
+            args.append('noskip')
+        
+        return '_'.join(args)
+    
+    @staticmethod
+    def decode(string_list):
+        """Decode a list of string notations to specify blocks inside the network.
+
+        Args:
+            string_list (list[str]): A list of strings, each string is a notation of block.
+        
+        Returns:
+            blocks_args: A list of BlockArgs namedtuples of block args.
+        """
+
+        assert isinstance(string_list, list)
+        blocks_args = []
+        for block_string in string_list:
+            blocks_args.append(BlockDecoder._decode_block_string(block_string))
+        return blocks_args
+    
+    @staticmethod
+    def encode(blocks_args):
+        """Encode a list of BlockArgs to a list of strings.
+        
+        Args:
+            blocks_args (list[namedtuples]): A list of BlockArgs manedtuples of block args.
+
+        Returns:
+            block_strings: A list of strings, each string is a notation of block.
+
+        """
+        block_strings = []
+        for block in blocks_args:
+            block_strings.append(BlockDecoder._encode_block_string(block))
+        return block_strings
+    
+
